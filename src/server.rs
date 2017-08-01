a75a95982e7e2bb61fd0a28871b2d0e190dd0277
viperscape <chrisfgill@gmail.com>
2017-08-01 12:07:07 -0400

    clients sessions and cookies

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,17 @@
+extern crate rand;
+extern crate cookie;
+
+use self::rand::random;
+use self::cookie::Cookie;
 use nickel::{Nickel, HttpRouter, FormBody};
+use nickel::extensions::Redirect;
+use nickel::Request;
 
 use std::collections::HashMap;
 use std::sync::{Arc,Mutex};
 use ldap::Ldap;
+
+pub type Clients = Arc<Mutex<HashMap<String,String>>>;
 
 pub struct Server {
     server: Nickel,
@@ -11,7 +20,7 @@ pub struct Server {
     ip: String,
     port: String,
 
-    clients: Arc<Mutex<HashMap<String,String>>>,
+    clients: Clients,
 }
 
 impl Server {
@@ -33,9 +42,19 @@ impl Server {
 
     fn apply_routes(&mut self) {
         let ad = Arc::new(self.ad.clone());
-        let clients = self.clients.clone();
         
-        self.server.get("/", middleware!("Hello World"));
+        // we must make BC happy
+        let clients_set_auth = self.clients.clone();
+        let clients_get_auth = self.clients.clone();
+        
+        self.server.get("/", middleware!
+                        { |req, res|
+                           if Server::is_auth(req, &clients_get_auth) {
+                               return res.redirect("/special")
+                           }
+                           
+                           return res.redirect("/login")
+                        });
 
         self.server.get("/login", middleware!
                         { |_rq, res|
@@ -46,22 +65,28 @@ impl Server {
                         });
 
         self.server.post("/auth", middleware!
-                         { |req, res|
+                         { |req, mut res|
                             let form_data = try_with!(res, req.form_body());
                             if let Some(username) = form_data.get("username") {
+                                let username = username.to_owned();
                                 if let Some(password) = form_data.get("password") {
                                     
                                     if !username.is_empty() &&
                                         !password.is_empty() {
                                             if ad.auth(&username,&password) {
-                                                if let Ok(mut clients) = clients.lock() {
-                                                    clients.insert(username.to_owned(),
-                                                                   "session".to_owned());
+                                                if let Ok(mut clients) = clients_set_auth.lock() {
+                                                    let sid = random::<u64>() .to_string();
+                                                    let cookie_sid = Cookie::new("sid", sid.clone()).to_string();
+                                                    let cookie_username = Cookie::new("username", username.clone()).to_string();
+                                                    clients.insert(username,
+                                                                  sid);
+                                                    
+                                                    res.headers_mut().set_raw("Set-Cookie",
+                                                                              vec![cookie_sid.as_bytes().to_vec(),
+                                                                                   cookie_username.as_bytes().to_vec()]);
                                                 }
                                                 
-                                                let mut data = HashMap::new();
-                                                data.insert("authenticated","true");
-                                                return res.render("views/auth.html", &data)
+                                                return res.redirect("/")
                                             }
                                         }
                                 }
@@ -83,5 +108,52 @@ impl Server {
         if let Ok(_s) = self.server.listen(&address) {
             // NOTE: we may detach this at some point!
         }
+    }
+
+    fn parse_cookies(raw: &[Vec<u8>]) -> Vec<Cookie> {
+        let mut cookies = vec!();
+        for cookies_raw in raw.iter() {
+            let cookies_str = String::from_utf8(cookies_raw.clone())
+                .expect("Non-utf8 encoding encountered");
+            for cookie_str in cookies_str.split(';') {
+                let s = cookie_str.trim().to_owned();
+                if let Ok(cookie) = Cookie::parse(s) {
+                    cookies.push(cookie);
+                }
+                
+            }
+        }
+
+        cookies
+    }
+
+    fn get_cookie<'a>(name: &str, cookies: &'a[Cookie]) -> Option<&'a str> {
+        for c in cookies {
+            let kv = c.name_value();
+            if kv.0 == name {
+                return Some(kv.1)
+            }
+        }
+
+        None
+    }
+
+    fn is_auth (req: &Request, cm: &Clients) -> bool {
+        if let Some(cookies) =  req.origin.headers.get_raw("Cookie") {
+            let cookies = Server::parse_cookies(cookies);
+            if let Some(username) = Server::get_cookie("username", &cookies) {
+                if let Some(sid) = Server::get_cookie("sid", &cookies) {
+                    if let Ok(cm) = cm.lock() {
+                        if let Some(c) = cm.get(username) {
+                            if c == sid {
+                                return true
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        false
     }
 }
